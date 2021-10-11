3e9a3dd081b79bb67b940a354693cd2cc0bd5332
Rishav Kumar <rishavkumar11111@gmail.com>
2021-10-11 19:13:01 +0530

    some changes

--- server/config/dbCredentials.js
+++ server/config/dbCredentials.js
@@ -1,9 +1,11 @@
 const dbCredentials = () => {
   const encodedUser = 'cG9zdGdyZXM=';
-  const encodedHost = 'bG9jYWxob3N0';
+  const encodedHost = 'bG9jYWxob3N0'; // local
+  // const encodedHost = 'MTAuMTcyLjAuNzg='; // staging
   const enCodedDatabase = 'TUJJbXBsZW1lbnRz';
-  const encodedPassword = 'c2FAMTIzIw==';
-  // const encodedPassword = 'MTIzNA==';
+  const encodedPassword = 'c2FAMTIzIw=='; // local Rishav
+  // const encodedPassword = 'MTIzNA=='; // local Sandeep
+  // const encodedPassword = 'cG9zdGdyZXM='; // staging
   const encodedPort = 'NTQzMg==';
   return {
     encodedUser, encodedHost, enCodedDatabase, encodedPassword, encodedPort
